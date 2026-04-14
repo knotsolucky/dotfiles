ecb1deee23d39e4d445acc6798bc0e8f459931b2
knotsolucky <2003habeeb@gmail.com>
2026-04-14 10:19:57 +0000

    another one

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -4,23 +4,12 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter").setup({
-        install_dir = vim.fn.stdpath("data") .. "/site",
-      })
-
       vim.api.nvim_create_autocmd("FileType", {
-        group = vim.api.nvim_create_augroup("user_ts", { clear = true }),
+        group = vim.api.nvim_create_augroup("user_ts_highlight", { clear = true }),
         callback = function(args)
           pcall(vim.treesitter.start, args.buf)
         end,
       })
-
-      local langs = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" }
-      if vim.fn.executable("tree-sitter") == 1 then
-        vim.schedule(function()
-          require("nvim-treesitter").install(langs)
-        end)
-      end
     end,
   },
 }
